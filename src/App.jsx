11ee22ea402babcf26bb6beb735ccd68be33a431
Foluso Ojo <98808180+FolusoOjo@users.noreply.github.com>
2024-12-02 10:50:33 +0100

    Revert "Wallet"

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,32 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./static/Header/Headerr";
-import Footer from "./static/Footer/Footer";
-import Homepage from "./pages/Homepage/Homepage";
-import Signup from "./components/Login-Signup/Signup";
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Header from "./static/Header/Headerr"
+import Footer from "./static/Footer/Footer"
+import Homepage from "./pages/Homepage/Homepage"
+import Signup from "./components/Login-Signup/Signup"
 
-import Verify from "./components/Login-Signup/Verify";
-import Login from "./components/Login-Signup/Login";
-import Wallet from "./components/Wallet/Wallet";
+import Verify from "./components/Login-Signup/Verify"
+import Login from "./components/Login-Signup/Login"
+
+
 
 function App() {
+
   return (
+
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/verify" element={<Verify />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/wallet" element={<Wallet />} />
-      </Routes>
-      <Footer />
+      <Header/>
+        <Routes>
+          <Route path="/" element={<Homepage/>}/>
+          <Route path="/verify" element={<Verify/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/login" element={<Login/>}/>
+
+        </Routes>
+      <Footer/>
     </BrowserRouter>
-  );
+
+  )
+  
 }
 
-export default App;
+export default App
--- src/static/Header/Headerr.jsx
+++ src/static/Header/Headerr.jsx
@@ -1,21 +1,22 @@
-import qlogo from "../../assets/png/logo.png";
+import qlogo from '../../assets/png/logo.png'
 import { IoIosArrowDown } from "react-icons/io";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { IoReorderTwoOutline } from "react-icons/io5";
-import Sidebar from "../Sidebar/SideBar";
-import { useState } from "react";
+import Sidebar from '../Sidebar/SideBar';
+import { useState } from 'react';
 
 const Header = () => {
   const [toggle, setToggle] = useState(false);
 
-  const handleToggle = () => {
-    setToggle(!toggle);
-  };
+        const handleToggle = () =>{
+            setToggle(!toggle)
+        }
 
   return (
     <>
-      <Wrapper>
+    <Wrapper>
+
         <Navbar>
           <Logocont>
             <img src={qlogo} alt="logo" />
@@ -23,157 +24,139 @@ const Header = () => {
           </Logocont>
 
           <Navlist>
-            <p>
-              <Link to="./">Home</Link>
-            </p>
-            <Dash>
-              Dashboard
-              <span>
-                <IoIosArrowDown />
-              </span>
-            </Dash>
-            <p>
-              <Link to="/wallet">Wallet</Link>
-            </p>
-            <p>About Us</p>
-            <p>Contact us</p>
+              <p><Link to= "./">Home</Link></p>
+              <p>Dashboard <IoIosArrowDown/> </p>
+              <p>Wallet</p>
+              <p>About Us</p>
+              <p>Contact us</p>
           </Navlist>
 
           <Signcont>
-            <Link to="/login">
-              <LoginButton LoginButton>Login </LoginButton>
-            </Link>
-
-            <Link to="/signup">
+              <Link to="/login">
+                  <LoginButton LoginButton>Login </LoginButton>
+              </Link>
+          
+              <Link to="/signup">
               <SigninButton>Sign up </SigninButton>
-            </Link>
+              </Link>
+            
           </Signcont>
 
-          <Sidenav onClick={handleToggle}>
-            <IoReorderTwoOutline />
-          </Sidenav>
+          <Sidenav onClick={handleToggle} ><IoReorderTwoOutline/></Sidenav>
+
         </Navbar>
-      </Wrapper>
 
-      {toggle && <Sidebar setDisplay={setToggle} />}
+        </Wrapper>
+        
+        {toggle && <Sidebar setDisplay={setToggle} />}
     </>
-  );
-};
+    
+  )
+}
 
-export default Header;
+export default Header
 
 const Wrapper = styled.div`
-  width: 100%;
-  height: 70px;
-  background-color: #1b6392;
-  display: flex;
-  align-items: center;
-`;
-const Navbar = styled.div`
-  max-width: 1800px;
-  width: 95%;
-  /* border: 1px solid white; */
-  margin: 0 auto;
-
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-  color: white;
-
-  @media (max-width: 768px) {
-    justify-content: space-between;
-  }
-`;
-const Signcont = styled.div`
-  display: flex;
-  gap: 20px;
-`;
-const Navlist = styled.div`
-  display: flex;
-  gap: 20px;
-  font-size: 12px;
-  cursor: pointer;
-
-  a {
-    text-decoration: none;
-    color: white;
-  }
-
-  @media (max-width: 768px) {
-    display: none;
-  }
-`;
-const Dash = styled.p`
-  display: flex;
-  justify-items: center;
-  align-items: center;
-
-  span {
+    width: 100%;
+    height: 70px;
+    background-color: #1B6392;
     display: flex;
-    justify-items: center;
     align-items: center;
-    margin-left: 5px;
+    
+`
+const Navbar = styled.div`
+    max-width: 1800px;
+    width: 95%;
+    /* border: 1px solid white; */
+    margin: 0 auto;
+
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    color: white;
+
+    @media (max-width: 768px) {
+      justify-content: space-between;
   }
-`;
+`
+const Signcont = styled.div`
+    display: flex;
+    gap: 20px;
+`
+const Navlist = styled.div`
+    display: flex;
+    gap: 20px;
+    font-size: 12px;
+    cursor: pointer;
+
+    a {
+      text-decoration: none;
+      color: white;
+    }
+
+    @media (max-width: 768px) {
+    display: none; 
+  }
+`
 const Logocont = styled.div`
-  display: flex;
-  color: white;
-  font-size: 28px;
-  gap: 10px;
-  cursor: pointer;
-
-  img {
-    width: 40px;
-  }
-`;
+    display: flex;
+    color: white;
+    font-size: 28px;
+    gap: 10px;
+    cursor: pointer;
+    
+    img{
+        width: 40px;
+    }
+`
 const LoginButton = styled.button`
-  background-color: transparent;
-  padding: 8px 24px;
-  border-radius: 4px;
-  border: 1px solid #fa8232;
-  color: white;
-  cursor: pointer;
-
-  &:hover {
-    background-color: #30506b;
-  }
-
-  a {
-    text-decoration: none;
+    background-color: transparent;
+    padding: 8px 24px;
+    border-radius: 4px;
+    border: 1px solid #FA8232;
     color: white;
-  }
+    cursor: pointer;
 
-  @media (max-width: 768px) {
-    display: none;
+    &:hover {
+      background-color: #30506b;
+    }
+
+    a {
+      text-decoration: none;
+      color: white;
+    }
+
+    @media (max-width: 768px) {
+    display: none; 
   }
-`;
+`
 const SigninButton = styled.button`
-  padding: 8px 24px;
-  border-radius: 4px;
-  background-color: #fa8232;
-  color: white;
-  border: none;
-  cursor: pointer;
-
-  &:hover {
-    background-color: #c96d05;
-  }
-
-  a {
-    text-decoration: none;
+    padding: 8px 24px;
+    border-radius: 4px;
+    background-color:  #FA8232;
     color: white;
-  }
+    border: none;
+    cursor: pointer;
 
-  @media (max-width: 768px) {
-    display: none;
+    &:hover {
+      background-color: #c96d05;
+    }
+
+    a {
+      text-decoration: none;
+      color: white;
+    }
+
+    @media (max-width: 768px) {
+    display: none; 
   }
-`;
+`
 const Sidenav = styled.div`
-  font-size: 40px;
-  display: none;
-  cursor: pointer;
-
-  @media (max-width: 768px) {
-    display: block;
-  }
-`;
+    font-size: 40px;
+    display: none;
+    cursor: pointer;
+    
+    @media (max-width: 768px){
+        display: block;
+    }
+`

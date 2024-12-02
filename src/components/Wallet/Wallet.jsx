01c2769fc751fa21726eaaa62ccacbfef861a751
Foluso Ojo <folusoojo96@gmail.com>
2024-12-02 10:42:04 +0100

    my wallet commit

--- src/components/Wallet/Wallet.jsx
+++ src/components/Wallet/Wallet.jsx
@@ -1,102 +1,91 @@
 import styled from "styled-components";
 import Card from "../../assets/svg/card.svg";
 import { IoMdArrowDropright } from "react-icons/io";
-import innerbg from '../../assets/png/innerbg.png'
+import innerbg from "../../assets/png/innerbg.png";
 
 function Wallet() {
   return (
     <>
-      <Bgcolor>
-        <Container>
-          <Wrapper>
-            <Flex>
-              <img src={Card} alt="ATM card" />
-              <Button>
-                <Add>
-                  <p>
-                    Add Money
-                    <IoMdArrowDropright size={25} />
-                  </p>
-                </Add>
-                <Withdraw>
-                  <p>Withdraw </p>
-                  <IoMdArrowDropright size={30} />
-                </Withdraw>
-              </Button>
-            </Flex>
-            <Text>
-              <Transact>
-                <p>Transaction History</p>
-                <View>View all</View>
-              </Transact>
-              <Transaction>
-                <Transfer>
-                  <div>
-                    <h4>Transfer to SAIL04 Supermarket</h4>
-                    <p>Nov 22nd, 18:40:26</p>
-                  </div>
-                  <div>
-                    <p>#30,000</p>
-                  </div>
-                </Transfer>
-                <hr />
+      <Container>
+        <Wrapper>
+          <Flex>
+            <img src={Card} alt="ATM card" />
+            <Button>
+              <Withdraw>
+                <span>Withdraw</span>
+                <span>
+                  <IoMdArrowDropright size={20} />
+                </span>
+              </Withdraw>
+            </Button>
+          </Flex>
 
-                <Transfer>
-                  <div>
-                    <h4>Transfer to EMMANUEL FROM WALLET</h4>
-                    <p>Nov 22nd, 18:40:26</p>
-                  </div>
-                  <div>
-                    <p>#100,000</p>
-                  </div>
-                </Transfer>
-                <hr />
+          <Text>
+            <Transact>
+              <p>Transaction History</p>
+              <View>View all</View>
+            </Transact>
+            <Transaction>
+              <Transfer>
+                <div>
+                  <h4>Transfer to SAIL04 Supermarket</h4>
+                  <p>Nov 22nd, 18:40:26</p>
+                </div>
+                <div>
+                  <p>#30,000</p>
+                </div>
+              </Transfer>
+              <hr />
 
-                <Transfer>
-                  <div>
-                    <h4>Transfer to SAIL04 Supermarket</h4>
-                    <p>Nov 22nd, 18:40:26</p>
-                  </div>
-                  <div>
-                    <p>#30,000</p>
-                  </div>
-                </Transfer>
-                <hr />
+              <Transfer>
+                <div>
+                  <h4>Transfer to EMMANUEL FROM WALLET</h4>
+                  <p>Nov 22nd, 18:40:26</p>
+                </div>
+                <div>
+                  <p>#100,000</p>
+                </div>
+              </Transfer>
+              <hr />
 
-                <Transfer>
-                  <div>
-                    <h4>Transfer to SAIL04 Supermarket</h4>
-                    <p>Nov 22nd, 18:40:26</p>
-                  </div>
-                  <div>
-                    <p>#30,000</p>
-                  </div>
-                </Transfer>
-                <hr />
-              </Transaction>
-            </Text>
-          </Wrapper>
-        </Container>
-      </Bgcolor>
+              <Transfer>
+                <div>
+                  <h4>Transfer to SAIL04 Supermarket</h4>
+                  <p>Nov 22nd, 18:40:26</p>
+                </div>
+                <div>
+                  <p>#30,000</p>
+                </div>
+              </Transfer>
+              <hr />
+
+              <Transfer>
+                <div>
+                  <h4>Transfer to SAIL04 Supermarket</h4>
+                  <p>Nov 22nd, 18:40:26</p>
+                </div>
+                <div>
+                  <p>#30,000</p>
+                </div>
+              </Transfer>
+              <hr />
+            </Transaction>
+          </Text>
+        </Wrapper>
+      </Container>
     </>
   );
 }
 export default Wallet;
 
-const Bgcolor = styled.div`
-  width: 100%;
-  background-image: url(${innerbg});
-  background-color: #edeff2;
-  background-size: contain;
-  background-position: top;
-  height: auto;
-`;
 const Container = styled.div`
+  background-color: skyblue;
   max-width: 1280px;
   width: 85%;
   margin: auto;
   padding: 0px 10px;
-  min-height: calc(100vh - 70px);
+  min-height: 100vh;
+  padding-bottom: 30px;
 `;
 const Wrapper = styled.div`
   /* border: 2px solid purple; */
@@ -107,55 +96,63 @@ const Flex = styled.div`
   /* border: 2px solid green; */
   padding: 20px 0;
   display: flex;
-  justify-content: space-between;
   align-items: center;
   img {
     width: 300px;
   }
+
+  @media (max-width: 800px) {
+    /* background-color: red; */
+    display: block;
+    text-align: center;
+
+    img {
+      width: 250px;
+    }
+  }
 `;
 const Button = styled.div`
   /* border: 2px solid yellow; */
-  width: 50%;
   display: flex;
-  justify-content: center;
-  margin-top: 60px;
-`;
-const Add = styled.button`
-  background-color: #ff3b30;
-  color: white;
-  border-color: transparent;
-  outline: none;
-  border-radius: 4px;
-  font-size: 15px;
-  width: 120px;
-  padding: 7px;
-  margin-right: 5px;
+  margin-left: 40px;
+  margin-top: 127px;
+  /* align-items: center; */
 
-  p {
-    text-align: center;
+  @media (max-width: 800px) {
+    margin-top: 10px;
+    margin-left: 0;
+    width: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
+    /* border: 2px solid blue; */
   }
 `;
+const Withdraw = styled.p`
+  display: flex;
+  justify-items: center;
+  align-items: center;
 
-const Withdraw = styled.button`
+  border-color: transparent;
+  /* width: 105px; */
+  /* height: 45px; */
+  margin-right: 5px;
   background-color: #fa8232;
   color: white;
-  border-color: transparent;
-  outline: none;
-  font-size: 15px;
-  padding: 7px;
-  margin-right: 5px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   border-radius: 4px;
-  p {
-    text-align: center;
+  padding: 10px;
+
+  span {
     display: flex;
-    justify-content: center;
+    justify-items: center;
     align-items: center;
+    font-size: 17px;
+  }
+
+  @media (max-width: 800px) {
+    span {
+      margin: auto;
+    }
   }
 `;
 
@@ -165,6 +162,12 @@ const Transact = styled.div`
   align-items: center;
   justify-content: space-between;
   /* border: 2px solid hotpink; */
+  font-size: 17px;
+
+  @media (max-width: 800px) {
+    /* background-color: yellow; */
+    margin-top: 15px;
+  }
 `;
 
 const Transaction = styled.div`
@@ -189,7 +192,6 @@ const Transfer = styled.div`
   margin: 5px 0;
   padding: 10px 5px;
   width: 80%;
-  font-size: 13px;
   div {
     h4 {
       margin-bottom: 4px;
